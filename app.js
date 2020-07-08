97a8cc6377cd802c23c56a5c884f3fbdf9afd1f9
Alejandro Madrazo <jmadrazo7@gmail.com>
2020-07-08 17:18:30 +0000

    final changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app1 = new Vue({
   el: '#app1',
   data: {
     title: "Hello Mundo",
+    title2: "title 2",
 
   },
   methods: {
@@ -11,8 +12,25 @@ var app1 = new Vue({
   },
 });
 
+// important we cannot add reactive  (common vue properties) properties like this
+app1.staticProperty = "I'm not reactive";
+console.log(app1.staticProperty.title);
+
+// adding real (reactive) properties
+
+// accesing data elements
+console.log(app1.$data.title);
+
+// accesing elements by ref
+console.log(app1.$refs.title2);
+
+setTimeout(function(){
+app1.$refs.title2.innerText = "hola perruno";
+
+},3000);
+
 var app2 = new Vue({
-  el: '#app2',
+  // no el property
   data: {
     title: "Hola World",
 
@@ -24,3 +42,13 @@ var app2 = new Vue({
     }
   },
 });
+// mounting instead of el property
+app2.$mount('#app2');
+
+
+// mounting templates
+var app3 = new Vue({
+  template: '<h1>Hello Gatuno</h1>'
+});
+
+//app3.$mount('#app3');
